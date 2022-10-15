4e327fab6f9d50149adf29c3d9e769bd98a68d7c
gusev76 <gusev76dn@mail.ru>
2022-10-15 23:45:25 +0300

    добавили папке WORK27

---
+++ WORK27/Program.cs
@@ -0,0 +1,40 @@
+﻿// задача 
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int GetSumAllDigits(int number)
+{
+    int result = 0;
+    while(number > 0)
+    {
+        result += number % 10;
+        number = number /10;
+    }
+    return result;
+}
+
+bool IsNumberValid(int number)
+{
+    if(number < 0)
+    return false;
+
+    return true;
+}
+
+int number = Prompt("Введите число: ");
+
+if(IsNumberValid(number))
+{
+    int SumDigits = GetSumAllDigits(number);
+    Console.WriteLine("Сумма всех цифр в числе = " + SumDigits);
+}
+else
+{
+    Console.WriteLine("Программф с такими числами не работает");
+}
+
