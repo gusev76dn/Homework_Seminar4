4f7057d9bd7fa54d10af534c23cbbdec004572d0
gusev76 <gusev76dn@mail.ru>
2022-10-16 00:55:59 +0300

    добавили папку WORK25

---
+++ WORK25/Program.cs
@@ -0,0 +1,20 @@
+﻿// задача 25
+
+int Exponentiation(int numberA, int numberB)
+{
+    int result = 1;
+    for(int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation = Exponentiation(numberA, numberB);
+Console.WriteLine("Ответ: " + Exponentiation);
